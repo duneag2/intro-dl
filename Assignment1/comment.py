1fb6fd9b8fbcd48572e7b2655d3900495c7f7009
Seungeun Lee <77534726+duneag2@users.noreply.github.com>
2024-03-13 20:05:12 +0900

    Update comment.py

--- Assignment1/comment.py
+++ Assignment1/comment.py
@@ -17,3 +17,5 @@ dot_product = torch.sum(matrix1 * matrix2)
 norm_product = torch.norm(matrix1) * torch.norm(matrix2)
 cos_sim = dot_product / norm_product
 print("Cosine similarity:", cos_sim.item())
+
+## PyTorch에서 L1, L2-norm과 cosine similarity는 다양한 방식으로 계산할 수 있어서 어떻게 방법으로 하셔도 상관은 크게 없습니다!
